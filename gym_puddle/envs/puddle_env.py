2dc6359f960128ae41fb0dbfcaf01f3c42df2306
daniel <1534513+dantp-ai@users.noreply.github.com>
2024-09-20 11:40:36 +0200

    Update render modes
     * Remove render (TODO: implement it using PyGame)

--- gym_puddle/envs/puddle_env.py
+++ gym_puddle/envs/puddle_env.py
@@ -6,11 +6,11 @@ import copy
 
 
 class PuddleEnv(gym.Env):
-    metadata = {'render.modes': ['human', 'rgb_array']}
+    metadata = {"render_modes": ["rgb_array"]}
 
     def __init__(self, start=[0.2, 0.4], goal=[1.0, 1.0], goal_threshold=0.1,
             noise=0.025, thrust=0.05, puddle_center=[[.3, .6], [.4, .5], [.8, .9]],
-            puddle_width=[[.1, .03], [.03, .1], [.03, .1]]):
+            puddle_width=[[.1, .03], [.03, .1], [.03, .1]], render_mode = None):
         self.start = np.array(start)
         self.goal = np.array(goal)
         self.goal_threshold = goal_threshold
@@ -27,7 +27,8 @@ class PuddleEnv(gym.Env):
             self.actions[i][i//2] = thrust * (i%2 * 2 - 1)
 
         self._seed()
-        self.viewer = None
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -62,55 +63,3 @@ class PuddleEnv(gym.Env):
         else:
             self.pos = copy.copy(self.start)
         return self.pos
-
-    def _render(self, mode='human', close=False):
-        if close:
-            if self.viewer is not None:
-                self.viewer.close()
-                self.viewer = None
-            return
-
-        screen_width = 600
-        screen_height = 400
-
-        if self.viewer is None:
-            from gymnasium.envs.classic_control import rendering
-            from gym_puddle.shapes.image import Image
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-
-            import pyglet
-            img_width = 100
-            img_height = 100
-            fformat = 'RGB'
-            pixels = np.zeros((img_width, img_height, len(fformat)))
-            for i in range(img_width):
-                for j in range(img_height):
-                    x = float(i)/img_width
-                    y = float(j)/img_height
-                    pixels[j,i,:] = self._get_reward(np.array([x,y]))
-
-            pixels -= pixels.min()
-            pixels *= 255./pixels.max()
-            pixels = np.floor(pixels)
-
-            img = pyglet.image.create(img_width, img_height)
-            img.format = fformat
-            data=[chr(int(pixel)) for pixel in pixels.flatten()]
-
-            img.set_data(fformat, img_width * len(fformat), ''.join(data))
-            bg_image = Image(img, screen_width, screen_height)
-            bg_image.set_color(1.0,1.0,1.0)
-
-            self.viewer.add_geom(bg_image)
-
-            thickness = 5
-            agent_polygon = rendering.FilledPolygon([(-thickness,-thickness),
-             (-thickness,thickness), (thickness,thickness), (thickness,-thickness)])
-            agent_polygon.set_color(0.0,1.0,0.0)
-            self.agenttrans = rendering.Transform()
-            agent_polygon.add_attr(self.agenttrans)
-            self.viewer.add_geom(agent_polygon)
-
-        self.agenttrans.set_translation(self.pos[0]*screen_width, self.pos[1]*screen_height)
-
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
