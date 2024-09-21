24ce580911fe0ff38fcaafe9d3f752c94929d902
daniel <1534513+dantp-ai@users.noreply.github.com>
2024-09-21 15:53:55 +0200

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(name="gym_puddle",
-      version="0.0.1",
-      install_requires=["gymnasium>=0.28.0,<0.29.0", "pyglet==2.0.17"],
+      version="0.0.2",
       python_requires=">=3.11",
 )
