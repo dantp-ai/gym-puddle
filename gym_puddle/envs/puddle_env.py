108656f96fe1e8653bb5e47e08fe6ea201a4de42
daniel <1534513+dantp-ai@users.noreply.github.com>
2024-09-27 12:41:07 +0200

    Raise ValueError if start or goal out of bounds

--- gym_puddle/envs/puddle_env.py
+++ gym_puddle/envs/puddle_env.py
@@ -33,10 +33,6 @@ class PuddleEnv(gym.Env):
         puddles: list[Puddle] | None = None,
         render_mode: str | None = None,
     ):
-        self.start = (
-            start if start is not None else np.array([0.2, 0.4], dtype=np.float32)
-        )
-        self.goal = goal if goal is not None else np.array([1.0, 1.0], dtype=np.float32)
         self.goal_threshold = goal_threshold
         self.noise = noise
         self.thrust = thrust
@@ -49,6 +45,18 @@ class PuddleEnv(gym.Env):
 
         self.action_space = spaces.Discrete(5)
         self.observation_space = spaces.Box(0.0, 1.0, shape=(2,))
+
+        self.start = (
+            start if start is not None else np.array([0.2, 0.4], dtype=np.float32)
+        )
+        self.goal = goal if goal is not None else np.array([1.0, 1.0], dtype=np.float32)
+
+        if not self.observation_space.contains(
+            self.start
+        ) or not self.observation_space.contains(self.goal):
+            raise ValueError(
+                "The `start` or `goal` is invalid. Only values in [0., 1.] are allowed."
+            )
 
         self.actions = [np.zeros(2, dtype=np.float32) for _ in range(5)]
         # [(-0.05, 0), (0.05, 0), (0, -0.05), (0, 0.05), (0, 0)]
